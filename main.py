95d1bb9278982c2a9316daa7f4812d94a5e7bc08
pranayjasrasaria <pranayjasrasaria@gmail.com>
2026-03-13 18:01:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+a=10
+b=12
+c=12
+
+print(a!=b)
+print(b!=c)
+
+a="Python"
+b="codingnal"
+
+if a!=b:
+    print(a,'and',b,'are different')
+
+
+a=4
+b=5
+
+if (a==1) != (b==5):
+    print("Hello")
+
+
+a=int(input("entre the number"))
+if a%2 != 0:
+    print(a,"Is not even number")
+
+
